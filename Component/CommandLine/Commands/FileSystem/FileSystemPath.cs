d53d946717260dcfbd24845082d0742a87d4a1f8
franck-gaspoz <franck.gaspoz@gmail.com>
2020-06-09 04:58:21 +0200

    improved file/path print colorization in file system commands

--- Component/CommandLine/Commands/FileSystem/FileSystemPath.cs
+++ Component/CommandLine/Commands/FileSystem/FileSystemPath.cs
@@ -10,8 +10,12 @@ namespace DotNetConsoleSdk.Component.CommandLine.Commands.FileSystem
     public abstract class FileSystemPath
     {
         public static string ErrorColorization = $"{Red}";
-        public static string DirectoryColorization = $"{Bdarkgreen}";
+        public static string NormalDirectoryColorization = $"{Blue}";
+        public static string WritableDirectoryColorization = $"{Bdarkgreen}{White}";
+        public static string SystemWritableDirectoryColorization = $"{Bdarkgreen}{Yellow}";
+        public static string SystemColorization = $"{Red}";
         public static string FileColorization = $"";
+        public static string ReadOnlyFileColorization = $"{Green}";
         public FileSystemInfo FileSystemInfo { get; protected set; }
 
         public string Error;
@@ -26,6 +30,11 @@ namespace DotNetConsoleSdk.Component.CommandLine.Commands.FileSystem
         public bool IsDirectory => FileSystemInfo.Attributes.HasFlag(FileAttributes.Directory);
         public bool IsFile => !IsDirectory;
         public bool HasError => Error != null;
+        public bool IsReadOnly => FileSystemInfo.Attributes.HasFlag(FileAttributes.ReadOnly);
+        public bool IsSystem => FileSystemInfo.Attributes.HasFlag(FileAttributes.System);
+        public bool IsHidden => FileSystemInfo.Attributes.HasFlag(FileAttributes.Hidden);
+        public bool IsArchive => FileSystemInfo.Attributes.HasFlag(FileAttributes.Archive);
+        public bool IsCompressed => FileSystemInfo.Attributes.HasFlag(FileAttributes.Compressed);
 
         public string GetError() => $"{ErrorColorization}{Error}";
 
@@ -39,18 +48,32 @@ namespace DotNetConsoleSdk.Component.CommandLine.Commands.FileSystem
 
         public void Print(bool printAttributes=false,bool shortPath=false,string prefix="",string postfix="")
         {
-            var color = (IsDirectory) ? DirectoryColorization : FileColorization;
+            var bg = GetCmd(KeyWords.b + "", DefaultBackground.ToString().ToLower());
+            var color = (IsDirectory) ? NormalDirectoryColorization : FileColorization;
+            if (!IsSystem && IsDirectory && !IsReadOnly) color += WritableDirectoryColorization;
+            if (IsSystem && !IsDirectory) color += SystemColorization + bg;
+            if (IsSystem && IsDirectory && !IsReadOnly) color += SystemWritableDirectoryColorization;
+            if (IsFile && IsReadOnly) color += ReadOnlyFileColorization;
             var r = "";
             var attr = "";
+            string hidden = "";
             if (printAttributes)
             {
-                var dir = IsDirectory ? " d" : "  ";
+                var dir = IsDirectory ? "d" : "-";
+                var ro = IsReadOnly ? "r-" : "rw";
+                var sys = IsSystem ? "s" : "-";
+                var h = IsHidden ? "h" : "-";
+                //var c = IsCompressed ? "c" : "-";
+                var a = IsArchive ? "a" : "-";
                 var size = (IsDirectory) ? "" : HumanFormatOfSize(((FileInfo)FileSystemInfo).Length, 2);
                 var moddat = FileSystemInfo.LastWriteTime;
+                hidden = IsHidden ? "*" : "";
                 var smoddat = $"{moddat.ToString("MMM", CultureInfo.InvariantCulture),-3} {moddat.Day,-2} {moddat.Hour.ToString().PadLeft(2,'0')}:{moddat.Minute.ToString().PadLeft(2,'0')}";
-                attr = $"{dir} {size,10} {smoddat}  ";
+                attr = $" {dir}{ro}{sys}{h}{a} {size,10} {smoddat}  ";
             }
-            r += $"{attr}{color}{prefix}{(shortPath?FileSystemInfo.Name:FileSystemInfo.FullName)}{postfix}";
+            var name = shortPath ? FileSystemInfo.Name : FileSystemInfo.FullName;
+            var quote =name.Contains(' ') ? "\"" : "";
+            r += $"{attr}{color}{prefix}{quote}{name}{quote}{hidden}{postfix}";
             DotNetConsole.Print(r);
             if (HasError)
                 DotNetConsole.Print($" {ErrorColorization}{GetError()}");
