0881710d0b91e001fa7da55d30df6622877853a1
franck-gaspoz <franck.gaspoz@gmail.com>
2020-06-10 01:47:27 +0200

    fixed message of wildcard path check exists (file OR directory doesn't exists)

--- Component/CommandLine/Commands/FileSystem/WildcardFilePath.cs
+++ Component/CommandLine/Commands/FileSystem/WildcardFilePath.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using static DotNetConsoleSdk.DotNetConsole;
 
 namespace DotNetConsoleSdk.Component.CommandLine.Commands.FileSystem
 {
@@ -31,6 +32,17 @@ namespace DotNetConsoleSdk.Component.CommandLine.Commands.FileSystem
                 }
             }
             DirectoryInfo = (DirectoryInfo)FileSystemInfo;
+        }
+
+        public override bool CheckExists(bool dumpError = true)
+        {
+            if (!DirectoryInfo.Exists)
+            {
+                if (dumpError)
+                    Errorln($"file or directory doesn't exists: {this}");
+                return false;
+            }
+            return true;
         }
 
         public static bool ContainsWildcardFileName(string path) {
